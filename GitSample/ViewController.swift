338dec36911d55c48f27a9f136a58a1b26038707
Wagdi <wagdic@gmail.com>
2019-06-29 03:47:32 +0300

    Initial

---
+++ GitSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitSample
+//
+//  Created by Wagdi Kala on 29/06/2019.
+//  Copyright © 2019 Wagdi Kala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
